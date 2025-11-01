6f6b26fd69403db67a54240f9efbc123ca0934a6
진창훈 <chj6966@gamil.com>
2025-11-01 16:04:42 +0900

    mypage 추가 작업

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
 import { MainCarousel } from "@/components/main-page-contents/MainCarousel";
 import PopularSection from "@/components/main-page-contents/PopularSection";
-import MyListings from "@/components/MyListings";
 export default function Home() {
   return (
     <div>
       <MainCarousel />
       <PopularSection />
-      <MyListings />
     </div>
   );
 }
--- src/components/mypage/panels/SalesPanel.jsx
+++ src/components/mypage/panels/SalesPanel.jsx
@@ -1,22 +1,28 @@
 "use client";
 
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetClose } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetClose,
+} from "@/components/ui/sheet";
 import { ChevronLeft } from "lucide-react";
 
 export default function SalesPanel({ onClose }) {
-    return (
-        <Sheet open={true} onOpenChange={onClose}>
-            <SheetContent side="right" className="w-full md:max-w-xl">
-                <SheetClose className="absolute top-4 left-4 cursor-pointer">
-                    <ChevronLeft />
-                </SheetClose>
-                <SheetHeader>
-                    <SheetTitle className="mt-1 text-lg">판매내역</SheetTitle>
-                </SheetHeader>
-                <div className="p-4 overflow-y-auto h-full">
-                    <p>여기는 판매내역 패널입니다.</p>
-                </div>
-            </SheetContent>
-        </Sheet>
-    );
+  return (
+    <Sheet open={true} onOpenChange={onClose}>
+      <SheetContent side="right" className="w-full md:max-w-xl z-[9999]">
+        <SheetClose className="absolute top-4 left-4 cursor-pointer z-[10000]">
+          <ChevronLeft />
+        </SheetClose>
+        <SheetHeader>
+          <SheetTitle className="mt-1 text-lg">판매내역</SheetTitle>
+        </SheetHeader>
+        <div className="p-4 overflow-y-auto h-full">
+          <p>여기는 판매내역 패널입니다.</p>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
 }
