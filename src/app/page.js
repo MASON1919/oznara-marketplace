07c6e1994bc992396fd50c00fc6513460d8f4d39
sunho <sunoc303@gmail.com>
2025-11-03 16:07:58 +0900

    temp

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
 import { MainCarousel } from "@/components/main-page-contents/MainCarousel";
 import PopularSection from "@/components/main-page-contents/PopularSection";
 import MyListings from "@/components/MyListings";
+import MySideBar from "@/components/mypage/MySidebar";
+import ProfilePanel from "@/components/mypage/ProfileCard";
 export default function Home() {
   return (
     <div>
       <MainCarousel />
       <PopularSection />
       <MyListings />
+      <ProfilePanel />
+      <MySideBar />
     </div>
   );
 }
