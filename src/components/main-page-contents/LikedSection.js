d9b4ffa76a91ac95f43c2ad6345d188b7b12e50d
sunho <sunoc303@gmail.com>
2025-11-17 11:35:59 +0900

    Liked section cached

--- src/components/main-page-contents/LikedSection.js
+++ src/components/main-page-contents/LikedSection.js
@@ -1,45 +1,58 @@
 import { prisma } from "@/lib/prisma";
 import { getS3Url } from "@/lib/s3";
+import { redis } from "@/lib/redis";
 import LikedCarousel from "./LikedCarousel";
 
 export default async function LikedSection() {
-  const likedListings = await prisma.listing.findMany({
-    where: {
-      deleted: false,
-      // 거래 완료된 상품 제외
-      NOT: {
-        transaction: {
-          some: {
-            status: "Completed",
+  const chached = await redis.get("likedListings");
+  if (chached) {
+    const { likedListings, s3Urls } = chached;
+    return (
+      <div className="my-4 px-4 flex flex-col items-start">
+        <LikedCarousel likedListings={likedListings} s3Urls={s3Urls} />
+      </div>
+    );
+  } else {
+    const likedListings = await prisma.listing.findMany({
+      where: {
+        deleted: false,
+        // 거래 완료된 상품 제외
+        NOT: {
+          transaction: {
+            some: {
+              status: "Completed",
+            },
           },
         },
       },
-    },
-    orderBy: { likeCount: "desc" }, // 찜 많은 순으로 정렬 (1위부터)
-    take: 12,
-    select: {
-      id: true,
-      title: true,
-      price: true,
-      likeCount: true,
-      createdAt: true,
-      listingImages: {
-        where: { isCover: true },
-        take: 1,
-        select: {
-          s3Key: true,
+      orderBy: { likeCount: "desc" }, // 찜 많은 순으로 정렬 (1위부터)
+      take: 12,
+      select: {
+        id: true,
+        title: true,
+        price: true,
+        likeCount: true,
+        createdAt: true,
+        listingImages: {
+          where: { isCover: true },
+          take: 1,
+          select: {
+            s3Key: true,
+          },
         },
       },
-    },
-  });
+    });
 
-  const s3Urls = likedListings.map((listing) => {
-    return getS3Url(listing.listingImages[0].s3Key);
-  });
+    const s3Urls = likedListings.map((listing) => {
+      return getS3Url(listing.listingImages[0].s3Key);
+    });
 
-  return (
-    <div className="my-4 px-4 flex flex-col items-start">
-      <LikedCarousel likedListings={likedListings} s3Urls={s3Urls} />
-    </div>
-  );
+    await redis.set("likedListings", { likedListings, s3Urls }, { ex: 60 * 5 });
+
+    return (
+      <div className="my-4 px-4 flex flex-col items-start">
+        <LikedCarousel likedListings={likedListings} s3Urls={s3Urls} />
+      </div>
+    );
+  }
 }
