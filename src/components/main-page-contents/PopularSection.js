cc250923e0160c49e14194a521a4a6d028e87d77
sunho <sunoc303@gmail.com>
2025-11-17 10:00:26 +0900

    popular section cached

--- src/components/main-page-contents/PopularSection.js
+++ src/components/main-page-contents/PopularSection.js
@@ -1,42 +1,54 @@
 import { prisma } from "@/lib/prisma";
 import { getS3Url } from "@/lib/s3";
 import PopularCarousel from "./PopularCarousel";
+import { redis } from "@/lib/redis";
 export default async function PopularSection() {
-  const popularListings = await prisma.listing.findMany({
-    where: {
-      deleted: false,
-      // 거래 완료된 상품 제외
-      NOT: {
-        transaction: {
-          some: {
-            status: "Completed",
+  const chached = await redis.get("popularListings");
+  if (chached) {
+    const { popularListings, s3Urls } = chached;
+    return (
+      <div className="my-4 px-4 flex flex-col items-start">
+        <PopularCarousel popularListings={popularListings} s3Urls={s3Urls} />
+      </div>
+    );
+  } else {
+    const popularListings = await prisma.listing.findMany({
+      where: {
+        deleted: false,
+        // 거래 완료된 상품 제외
+        NOT: {
+          transaction: {
+            some: {
+              status: "Completed",
+            },
           },
         },
       },
-    },
-    orderBy: { viewCount: "desc" },
-    take: 12,
-    select: {
-      id: true,
-      title: true,
-      price: true,
-      viewCount: true,
-      createdAt: true,
-      listingImages: {
-        where: { isCover: true },
-        take: 1,
-        select: {
-          s3Key: true,
+      orderBy: { viewCount: "desc" },
+      take: 12,
+      select: {
+        id: true,
+        title: true,
+        price: true,
+        viewCount: true,
+        createdAt: true,
+        listingImages: {
+          where: { isCover: true },
+          take: 1,
+          select: {
+            s3Key: true,
+          },
         },
       },
-    },
-  });
-  const s3Url3 = popularListings.map((listing) => {
-    return getS3Url(listing.listingImages[0].s3Key);
-  });
-  return (
-    <div className="my-4 px-4 flex flex-col items-start">
-      <PopularCarousel popularListings={popularListings} s3Urls={s3Url3} />
-    </div>
-  );
+    });
+    const s3Urls = popularListings.map((listing) => {
+      return getS3Url(listing.listingImages[0].s3Key);
+    });
+    await redis.set("popularListings", { popularListings, s3Urls }, { ex: 5 });
+    return (
+      <div className="my-4 px-4 flex flex-col items-start">
+        <PopularCarousel popularListings={popularListings} s3Urls={s3Urls} />
+      </div>
+    );
+  }
 }
